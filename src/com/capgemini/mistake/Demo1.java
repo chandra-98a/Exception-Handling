f787d02caa1f0520f90d499eedf899442f4e0808
chandra-98a <chandrakalaa2022@gmail.com>
2023-09-15 10:44:03 +0530

    "Arithmetic Exception: / by zero"

--- src/com/capgemini/mistake/Demo1.java
+++ src/com/capgemini/mistake/Demo1.java
@@ -1,2 +1,19 @@
-package com.capgemini.mistake;public class Demo1 {
+package com.capgemini.mistake;
+
+import java.util.Scanner;
+
+public class Demo1 {
+    public static void main(String [] args){
+        Scanner scan=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("enter the value for a");
+        a=scan.nextInt();
+        System.out.println("enter the value for b");
+        b=scan.nextInt();
+
+        c=a/b;
+
+        System.out.println("the value of c is "+c);
+        System.out.println("Exiting main normally");
+    }
 }
